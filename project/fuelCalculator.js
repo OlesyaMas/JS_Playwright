3d094db484697e13f8d4a8a050383b1e40bde831
Olesya <maslyuk.lesya@gmail.com>
2024-01-21 16:58:00 +0200

    task 3 with calculation of car fuel

---
+++ project/fuelCalculator.js
@@ -0,0 +1,15 @@
+let carFuel1 = "disel";
+let carSize1 = "S";
+
+calcFuel(carSize1, carFuel1);
+
+let carFuel2 = "gas";
+let carSize2 = "XL";
+
+calcFuel(carSize2, carFuel2);
+
+function calcFuel(carSize, carFuel) {
+    console.log(`заправка для ${carSize} авто тип палива ${carFuel}`)
+}
+
+
